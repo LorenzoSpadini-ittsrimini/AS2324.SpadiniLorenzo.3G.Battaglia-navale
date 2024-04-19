8d641124a70d76b1700ed2c3aea8e0c83a5677a8
lorenzo <lorenzo@LAPTOP-E8F845EH>
2024-04-19 09:26:11 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,24 @@
+﻿namespace Battaglia_navale
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+     
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace Battaglia_navale
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
